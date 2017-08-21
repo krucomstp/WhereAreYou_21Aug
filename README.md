6f1c077ea3281c4acb5770854ade7bea3ac30edb
KruNamkang <kanyarat@suratham.ac.th>
2017-08-21 14:57:08 +0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+Application ที่ทำงานบน Wear โดยจะมีการ Random โค้ด
+ถ้ามีการ Add code จะเก็บไว้ใน SQLite
+สามารถ Get Lat, Lng ไปเก็บ บน Server ได้
+
+[![Screen Shot 2560-07-13 at 02.40.12.png](https://s1.postimg.org/wcj2kw7vz/Screen_Shot_2560-07-13_at_02.40.12.png)](https://postimg.org/image/dk77hbbhn/)
+
+[![Screen Shot 2560-07-13 at 02.40.57.png](https://s11.postimg.org/jb3cue0gz/Screen_Shot_2560-07-13_at_02.40.57.png)](https://postimg.org/image/dzog9oee7/)
+
+[![Screen Shot 2560-07-13 at 02.42.50.png](https://s9.postimg.org/51cl3w727/Screen_Shot_2560-07-13_at_02.42.50.png)](https://postimg.org/image/dwdfeevuj/)
+
+[![Screen Shot 2560-07-13 at 02.45.16.png](https://s12.postimg.org/l0i06tvgt/Screen_Shot_2560-07-13_at_02.45.16.png)](https://postimg.org/image/fcbpfxr49/)
+
+## ต้องการรายละเอียดเพิ่ม หรือ ต้องการ ปรึกษาการทำ โปรเจคแอนดรอยด์  
+
+### ติดต่อมาสเตอร์ อึ่ง เลย ที่  
+
+[![193820_10150134620804173_122194109172_6371843_68.jpg](https://s21.postimg.org/4i5tymwsn/193820_10150134620804173_122194109172_6371843_68.jpg)](https://postimg.org/image/4i5tymwsj/)
+
+  https://www.facebook.com/android.Training.by.EWTC
+
+  http://line.me/ti/p/XI-Ksj7Jzq  
+
+[![Line-QR.jpg](https://s9.postimg.org/41ec4gb3z/Line-_QR.jpg)](https://postimg.org/image/h5jwh535n/)
+
+  http://androidthai.in.th
+
+  phrombutr@gmail.com  
+
+Mobile 0818593550
---
+++ wear/src/main/java/mind/chan/mean/watchchild/MainActivity.java
@@ -0,0 +1,46 @@
+package mind.chan.mean.watchchild;
+
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.app.ActivityCompat;
+import android.support.wearable.view.WatchViewStub;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.util.Random;
+
+public class MainActivity extends Activity {
+
+    private TextView mTextView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        showText("Master UNG");
+
+    }   // Main Method
+
+    private void showText(final String strShowText) {
+        final WatchViewStub stub = (WatchViewStub) findViewById(R.id.watch_view_stub);
+        stub.setOnLayoutInflatedListener(new WatchViewStub.OnLayoutInflatedListener() {
+            @Override
+            public void onLayoutInflated(WatchViewStub stub) {
+                mTextView = (TextView) stub.findViewById(R.id.text);
+                mTextView.setText(strShowText);
+            }
+        });
+    }
+
+
+}   // Main Class
